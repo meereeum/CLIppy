24f31faf2ecdef5656529744ecbe3a1a106b8a56
Miriam Shiffman <meereeum@gmail.com>
2018-04-12 17:46:28 -0400

    fail gracefully wrapper

--- CLIppy/__init__.py
+++ CLIppy/__init__.py
@@ -1,4 +1,4 @@
 from .dateutils import convert_date
 from .netutils import safe_encode, soup_me
 from .printutils import pprint_header_with_lines
-from .utils import dedupe, flatten, get_from_file
+from .utils import dedupe, fail_gracefully, flatten, get_from_file
--- CLIppy/utils.py
+++ CLIppy/utils.py
@@ -1,5 +1,7 @@
+from functools import wraps
 import itertools
 import os
+import sys
 
 
 # via https://stackoverflow.com/questions/6197409/ordered-sets-python-2-7
@@ -7,6 +9,16 @@ def dedupe(lst):
     """list -> deduped iterable of items, in order"""
     from collections import OrderedDict
     return OrderedDict((item, None) for item in lst).keys()
+
+
+def fail_gracefully(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            f(*args, **kwargs)
+        except(KeyboardInterrupt):
+            sys.exit(0)
+    return wrapper
 
 
 def flatten(itable):
